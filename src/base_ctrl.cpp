1458700468c4e6f3b181f00317bd2b3c06bb8909
Garegin Margaryan <margaryang@gmail.com>
2018-08-23 19:25:52 +0200

    added base_ctrl class

---
+++ src/base_ctrl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Garegin Margaryan on 8/23/18.
+//
+
+#include "base_ctrl.h"
---
+++ src/base_ctrl.h
@@ -0,0 +1,14 @@
+//
+// Created by Garegin Margaryan on 8/23/18.
+//
+
+#ifndef MICRO_CPP_BASE_CTRL_H
+#define MICRO_CPP_BASE_CTRL_H
+
+
+class base_ctrl {
+
+};
+
+
+#endif //MICRO_CPP_BASE_CTRL_H
